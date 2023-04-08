d2c0b789fd8dc43791521d2c25636d28a247a565
Aman <you@example.com>
2023-04-08 17:18:22 +0530

    Crypto-Whale

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 //Creating API service to fetch data from Public API.
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '2d994de663mshf9dea2178bab0bcp1e13bdjsne37bf6408ce0'
+    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com'
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const cryptoNewsHeaders = {
         'X-BingApis-SDK': 'true',
         'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-        'X-RapidAPI-Key': '2d994de663mshf9dea2178bab0bcp1e13bdjsne37bf6408ce0'
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
 }
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
